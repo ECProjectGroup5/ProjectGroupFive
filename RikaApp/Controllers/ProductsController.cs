8013fb13e00dd81019ab80a9e6716e222145fb25
Ludvig <ludvig.vinoy@hotmail.com>
2024-11-06 21:12:22 +0100

    ProductDetailsPage working with localhost api. no styling

--- RikaApp/Controllers/ProductsController.cs
+++ RikaApp/Controllers/ProductsController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using RikaApp.ViewModels;
+using System.Text.Json.Nodes;
+using System.Text;
 
 namespace RikaApp.Controllers;
 
@@ -29,8 +31,36 @@ public class ProductsController(HttpClient client) : Controller
     }
 
     [HttpGet]
-    public IActionResult Details(Guid articleNumber)
+    public async Task <IActionResult> Details(Guid articleNumber)
     {
+        try
+        {
+            var viewModel = new ProductsViewModel();
+            var productRequest = new { ArticleNumber = articleNumber };
+
+            var body = JsonConvert.SerializeObject(productRequest);
+            var content = new StringContent(body, Encoding.UTF8, "application/json");
+
+            if (body != null)
+            {
+                var response = await _client.PostAsync("http://localhost:7189/api/GetOneByArticlenumberrAPI", content);
+
+                if (response != null && response.IsSuccessStatusCode)
+                {
+                    var rawProduct = await response.Content.ReadAsStringAsync();
+                    var unpackedProduct = JsonConvert.DeserializeObject<ProductModel>(rawProduct);
+
+                    viewModel.Product = unpackedProduct;
+                    return View(viewModel);
+                }
+            }
+           
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
         return View();
+      
     }
 }
