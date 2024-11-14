79ed4bc33c30c8ebe92ee5f3ffff974a675e3bdc
ibrahim fakhro <ibrahim.fakhro@live.se>
2024-11-14 21:45:52 +0100

    CategorieChanges

--- RikaApp/Controllers/CategoriesController.cs
+++ RikaApp/Controllers/CategoriesController.cs
@@ -4,7 +4,20 @@ namespace RikaApp.Controllers
 {
     public class CategoriesController : Controller
     {
+        [Route("/categories")]
+
         public IActionResult Index()
+        {
+            return View();
+
+        }
+        [Route("/categories2")]
+        public IActionResult Page2()
+        {
+            return View();
+        }
+        [Route("/categories3")]
+        public IActionResult Page3()
         {
             return View();
         }
